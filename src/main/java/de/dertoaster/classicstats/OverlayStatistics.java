4a51ade9792ca2ecb5c01e0ec92d9d0d1f64e981
DerToaster98 <seiler.oli@web.de>
2021-10-11 18:03:36 +0200

    the rest of the entire mod

---
+++ src/main/java/de/dertoaster/classicstats/OverlayStatistics.java
@@ -0,0 +1,36 @@
+package de.dertoaster.classicstats;
+
+import com.mojang.blaze3d.matrix.MatrixStack;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.AbstractGui;
+import net.minecraft.client.gui.FontRenderer;
+
+public class OverlayStatistics extends AbstractGui {
+
+	private Minecraft mcClient;
+
+	public OverlayStatistics(Minecraft minecraft) {
+		super();
+		this.mcClient = minecraft;
+	}
+
+	public void render(MatrixStack matrixStack) {
+		if (this.mcClient.options.renderDebug) {
+			return;
+		}
+		
+		final int score = this.mcClient.player.getScore();
+		final int xpNeededForNextLevel = this.mcClient.player.getXpNeededForNextLevel();
+		final int xp = this.mcClient.player.totalExperience;
+		final long day = this.mcClient.level.getDayTime() / 24000L;
+		
+		/*
+		 * Matrix, String, X, Y, color(?)
+		 */
+		 this.mcClient.font.draw(matrixStack, "Day: " + day, 2.0F, (float)2, 14737632);
+		 this.mcClient.font.draw(matrixStack, "Score: " + score, 2.0F, (float)20, 14737632);
+		 this.mcClient.font.draw(matrixStack, "XP: " + xp + "/" + xpNeededForNextLevel, 2.0F, (float)31, 14737632);
+	}
+
+}
---
+++ src/main/java/de/dertoaster/classicstats/RenderGUIHandler.java
@@ -0,0 +1,23 @@
+package de.dertoaster.classicstats;
+
+
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber
+@OnlyIn(Dist.CLIENT)
+public class RenderGUIHandler {
+	
+	protected static final Minecraft mc = Minecraft.getInstance();
+	protected static final OverlayStatistics overlay = new OverlayStatistics(mc);
+	
+	@SubscribeEvent
+	public static void onRenderTick(RenderGameOverlayEvent.Post event){
+		overlay.render(event.getMatrixStack());
+	}
+	
+}

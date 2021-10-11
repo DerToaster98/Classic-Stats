93c2fc42c8e64204285f9124f27d161e5a9e98b2
DerToaster98 <seiler.oli@web.de>
2021-10-11 18:32:41 +0200

    add stat device item

--- src/main/java/de/dertoaster/classicstats/ClassicStatsMod.java
+++ src/main/java/de/dertoaster/classicstats/ClassicStatsMod.java
@@ -1,14 +1,19 @@
 package de.dertoaster.classicstats;
 
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("classicstats")
+@Mod(ClassicStatsMod.MODID)
 public class ClassicStatsMod {
+	
+	public static final String MODID = "classicstats";
 
 	public ClassicStatsMod() {
-		// Register ourselves for server and other game events we are interested in
+		IEventBus modbus = FMLJavaModLoadingContext.get().getModEventBus();
+		CSItems.ITEMS.register(modbus);
 		MinecraftForge.EVENT_BUS.register(this);
 	}
 
--- src/main/java/de/dertoaster/classicstats/RenderGUIHandler.java
+++ src/main/java/de/dertoaster/classicstats/RenderGUIHandler.java
@@ -1,7 +1,11 @@
 package de.dertoaster.classicstats;
 
+import java.util.HashSet;
+import java.util.Set;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.player.ClientPlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
@@ -11,13 +15,30 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber
 @OnlyIn(Dist.CLIENT)
 public class RenderGUIHandler {
-	
+
 	protected static final Minecraft mc = Minecraft.getInstance();
 	protected static final OverlayStatistics overlay = new OverlayStatistics(mc);
-	
-	@SubscribeEvent
-	public static void onRenderTick(RenderGameOverlayEvent.Post event){
-		overlay.render(event.getMatrixStack());
+
+	protected static Set<Item> DEVICES = null;
+
+	protected static void fillDevices() {
+		DEVICES = new HashSet<Item>();
+		DEVICES.add(CSItems.ITEM_STAT_DEVICE.get());
 	}
-	
+
+	@SubscribeEvent
+	public static void onRenderTick(RenderGameOverlayEvent.Post event) {
+		if (DEVICES == null) {
+			fillDevices();
+		}
+
+		ClientPlayerEntity clientPlayer = mc.player;
+
+		boolean hasDevice = clientPlayer.inventory.hasAnyOf(DEVICES);
+
+		if (hasDevice) {
+			overlay.render(event.getMatrixStack());
+		}
+	}
+
 }

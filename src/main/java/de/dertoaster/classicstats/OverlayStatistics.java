001bedf71b82ee44a65d832618f25935d8943429
DerToaster98 <seiler.oli@web.de>
2021-10-11 18:08:25 +0200

    organize imports

--- src/main/java/de/dertoaster/classicstats/OverlayStatistics.java
+++ src/main/java/de/dertoaster/classicstats/OverlayStatistics.java
@@ -4,7 +4,6 @@ import com.mojang.blaze3d.matrix.MatrixStack;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.AbstractGui;
-import net.minecraft.client.gui.FontRenderer;
 
 public class OverlayStatistics extends AbstractGui {
 

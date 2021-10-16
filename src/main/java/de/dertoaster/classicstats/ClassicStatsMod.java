01c3e06d5fc448d89da56a3e6713cb730d0e3181
DerToaster98 <seiler.oli@web.de>
2021-10-16 12:15:09 +0200

    remove item

--- src/main/java/de/dertoaster/classicstats/ClassicStatsMod.java
+++ src/main/java/de/dertoaster/classicstats/ClassicStatsMod.java
@@ -1,9 +1,7 @@
 package de.dertoaster.classicstats;
 
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(ClassicStatsMod.MODID)
@@ -12,8 +10,6 @@ public class ClassicStatsMod {
 	public static final String MODID = "classicstats";
 
 	public ClassicStatsMod() {
-		IEventBus modbus = FMLJavaModLoadingContext.get().getModEventBus();
-		CSItems.ITEMS.register(modbus);
 		MinecraftForge.EVENT_BUS.register(this);
 	}
 
--- src/main/java/de/dertoaster/classicstats/RenderGUIHandler.java
+++ src/main/java/de/dertoaster/classicstats/RenderGUIHandler.java
@@ -1,10 +1,6 @@
 package de.dertoaster.classicstats;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import net.minecraft.client.Minecraft;
-import net.minecraft.item.Item;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
@@ -17,25 +13,6 @@ public class RenderGUIHandler {
 
 	protected static final Minecraft mc = Minecraft.getInstance();
 	protected static final OverlayStatistics overlay = new OverlayStatistics(mc);
-
-	protected static Set<Item> DEVICES = null;
-
-	protected static void fillDevices() {
-		DEVICES = new HashSet<Item>();
-		DEVICES.add(CSItems.ITEM_STAT_DEVICE.get());
-	}
-
-	/*@SubscribeEvent
-	public static void onRenderTick(RenderGameOverlayEvent.Post event) {
-		if (DEVICES == null) { fillDevices(); }
-		 
-		ClientPlayerEntity clientPlayer = mc.player;
-		 
-	 	boolean hasDevice = clientPlayer.inventory.hasAnyOf(DEVICES);
-		 
-	 	if (hasDevice) { overlay.render(event.getMatrixStack()); }
-		 
-	}*/
 
 	@SubscribeEvent
 	public static void onGameOverlayRender(RenderGameOverlayEvent event) {

dcd3ffa28760d9ff8646eabe25a12e232e787791
DerToaster98 <seiler.oli@web.de>
2021-10-16 12:21:16 +0200

    add yellow color to values

--- src/main/java/de/dertoaster/classicstats/OverlayStatistics.java
+++ src/main/java/de/dertoaster/classicstats/OverlayStatistics.java
@@ -1,9 +1,12 @@
 package de.dertoaster.classicstats;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.systems.RenderSystem;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.AbstractGui;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
 
 public class OverlayStatistics extends AbstractGui {
 
@@ -18,18 +21,21 @@ public class OverlayStatistics extends AbstractGui {
 		if (this.mcClient.options.renderDebug) {
 			return;
 		}
-		
+		RenderSystem.pushMatrix();
+
 		final int score = this.mcClient.player.getScore();
 		final int xpNeededForNextLevel = this.mcClient.player.getXpNeededForNextLevel();
 		final int xp = Math.round(this.mcClient.player.experienceProgress * xpNeededForNextLevel);
 		final long day = 1 + (this.mcClient.level.getDayTime() / 24000L);
-		
+
 		/*
 		 * Matrix, String, X, Y, color(?)
 		 */
-		 this.mcClient.font.draw(matrixStack, "Day: " + day, 2.0F, (float)2, 14737632);
-		 this.mcClient.font.draw(matrixStack, "Score: " + score, 2.0F, (float)20, 14737632);
-		 this.mcClient.font.draw(matrixStack, "XP: " + xp + "/" + xpNeededForNextLevel, 2.0F, (float)31, 14737632);
+		AbstractGui.drawString(matrixStack, this.mcClient.font, new StringTextComponent("Day: " + TextFormatting.YELLOW + day), 2, 2, -1);
+		AbstractGui.drawString(matrixStack, this.mcClient.font, "Score: " + TextFormatting.YELLOW + +score, 2, 20, -1);
+		AbstractGui.drawString(matrixStack, this.mcClient.font, "XP: " + TextFormatting.YELLOW + +xp + "/" + xpNeededForNextLevel, 2, 31, -1);
+
+		RenderSystem.popMatrix();
 	}
 
 }
